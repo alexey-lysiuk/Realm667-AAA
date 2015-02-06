7b11e9cfab69a413f20cd1be37f184f6e6b96cc2
alexey.lysiuk <alexey.lysiuk@gmail.com>
2015-02-06 14:20:40 +0200

    + enable other source/styles weapons

--- lib/patching.py
+++ lib/patching.py
@@ -72,9 +72,29 @@ def apply_patch_228(wad): # Zombieman Rifle
         r'(actor\s+)Rifle(\s*:\s*\w+)',
         r'\1ZombiemanRifle\2')
 
+def apply_patch_230(wad): # Plasma Gun
+    # fix class name collision with #329 Plasma Shotgun
+    replace_in_decorate(wad,
+        r'([^\w])PlasmaTrail([^\w])',
+        r'\1D3PlasmaTrail\2')
+
+def apply_patch_241(wad): # Devastator
+    # fix incorrect sprite
+    replace_in_decorate(wad, r'(\s)DVST(\s)', r'\1DVGG\2')
+
 def apply_patch_242(wad): # Freeze Rifle
     # fix incorrect sprite
-    replace_in_decorate(wad, 'PLSG', 'FRSG')
+    replace_in_decorate(wad, r'(\s)PLSG(\s)', r'\1FRSG\2')
+
+def apply_patch_271(wad): # Saw Thrower
+    # fix incorrect sprite
+    replace_in_decorate(wad,
+        r'Inventory.Icon(\s)SAWA',
+        r'Inventory.Icon\1SAWAA0')
+
+def apply_patch_314(wad): # Revolver PS
+    # fix incorrect sprite
+    replace_in_decorate(wad, r'HGUN(\s+)A(\s+)1', r'HGUN\1C\2-1')
 
 def apply_patch_372(wad): # Autogun
     replace_in_lump('GLDEFS', wad, 'PlickerLight', 'FlickerLight')
@@ -129,18 +149,30 @@ def apply_patch_817(wad): # Arbalest of the Ancients
         r'\1Arbalest')
 
 
-setslot_keyconfs = [
+broken_keyconfs = [
+    226, # Sawed Off
     235, # Uber Minigun
+    240, # Seeker Bazooka
+    241, # Devastator
     246, # EgoSmasher
+    252, # Tesla Cannon
+    257, # Power Nailgun
+    263, # Hunter Shotgun
+    266, # Napalm Launcher
+    267, # Necrovision MG40
+    269, # Pulse Machinegun
     273, # Swat Shotgun
     274, # Western Shotgun
+    283, # TommyGun
     308, # Doom 2.5 SSG
+    314, # Revolver PS
     328, # Spellbinder
     329, # Plasma Shotgun
     330, # Butchergun Chaingun
     509, # Prox Launcher
     510, # Vile Staff
     521, # Mag .60
+    536, # Jackbomb
     626, # D'sparil Staff
 ]
 
@@ -153,8 +185,8 @@ def apply_patch(id, wad):
     if no_doomednum:
         replace_in_decorate(wad, r'(actor\s+[\w\.]+\s*:\s*[\w\.]+\s+(replaces\s+[\w\.]+)?)\s*\d*', r'\1')
 
-    # Fix weapon slot resetting
-    if id in setslot_keyconfs:
+    # Fix weapon slot and player class resetting
+    if id in broken_keyconfs:
         remove_lump(wad, 'KEYCONF')
 
     func_name = 'apply_patch_{0}'.format(id)

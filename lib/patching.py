82a6eec0b34c55966077a393d5b285bd86ec0b29
alexey.lysiuk <alexey.lysiuk@gmail.com>
2015-02-16 11:03:53 +0200

    * fix doomnum removal for actors without parent

--- lib/patching.py
+++ lib/patching.py
@@ -385,7 +385,7 @@ def apply_patch(id, wad):
     if no_class_replacement:
         replace_in_decorate(wad, r'(actor\s+[\w~.]+\s*:\s*[\w~.]+)\s+replaces\s+[\w~.]+', r'\1')
     if no_doomednum:
-        replace_in_decorate(wad, r'(actor\s+[\w~.]+\s*:\s*[\w~.]+\s+(replaces\s+[\w~.]+)?)\s*\d*', r'\1')
+        replace_in_decorate(wad, r'(actor\s+[\w~.]+(\s*:\s*[\w~.]+)?\s+(replaces\s+[\w~.]+)?)\s*\d*', r'\1')
 
     make_unique_sprites(wad)
 

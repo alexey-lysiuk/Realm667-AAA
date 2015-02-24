196354698a877298a23e4809796b4747f40c5a44
alexey.lysiuk <alexey.lysiuk@gmail.com>
2015-02-24 17:37:37 +0200

    * fix bug in sprite renaming of inventory icon images

--- lib/patching.py
+++ lib/patching.py
@@ -154,11 +154,11 @@ def _generate_new_sprite_name(sprite, frames):
     return new_name
 
 def rename_sprite(wad, old, new):
-    search_pattern = r'(\s)%s(\w{0,4}[\s"])' % old
+    search_pattern = r'([\s"])%s(\w{0,4}[\s"])' % old
     replace_pattern = r'\g<1>{0}\g<2>'.format(new)
 
     replace_in_decorate(wad,
-        r'(\s){0}(\s)'.format(old), replace_pattern)
+        search_pattern, replace_pattern)
     replace_in_lump('ANIMDEFS', wad,
         search_pattern, replace_pattern, optional = True)
     replace_in_lump('DECALDEF', wad,

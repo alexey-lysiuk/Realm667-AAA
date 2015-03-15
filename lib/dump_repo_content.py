22668bc2f1f9e5508c9d3c6c851ffef5b2c8ea16
alexey.lysiuk <alexey.lysiuk@gmail.com>
2015-03-15 12:17:27 +0200

    + script to create repository's table of content

---
+++ lib/dump_repo_content.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+#
+# Realm667 - An Awesome Awesomeness
+# Copyright (C) 2015 Alexey Lysiuk
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+from repo import repository
+
+print('|ID|Name|Comment|\n|---|---|---|')
+
+for item in repository:
+    gid     = item[0]
+    name    = item[1]
+
+    if gid < 0:
+        gid     = -gid
+        comment = 'Excluded'
+    else:
+        comment = ''
+
+    link = '[{0:d}](https://github.com/alexey-lysiuk/Realm667-AAA-Cache/raw/master/{0:04d}.zip)'.format(gid)
+
+    print('|{}|{}|{}|'.format(link, name, comment))

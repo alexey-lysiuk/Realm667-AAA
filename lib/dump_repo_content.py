cf1afd6f6ee66d33cab537356f336d43cfbebff7
alexey.lysiuk <alexey.lysiuk@gmail.com>
2015-03-25 11:18:13 +0200

    * split content list by categories in dump repo script

--- lib/dump_repo_content.py
+++ lib/dump_repo_content.py
@@ -18,33 +18,78 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
+import os
+import shutil
+
+import utils
+
 from repo import REPOSITORY
 from web_repo import fetch_repository
 
-remote_repo = fetch_repository()
 
-print('\n|ID|Name|Preview|Comment|\n|---|---|---|---|')
+remote_repo = fetch_repository()
+toc = []
+
+toc_path = utils.cache_path + 'toc/'
+shutil.rmtree(toc_path)
+os.makedirs(toc_path)
+
+outfile = None
+
+
+def end_category():
+    if outfile:
+        outfile.write('\n[Back to table of content](../readme.md)\n')
+        outfile.close()
+
 
 for item in REPOSITORY:
     gid = item[0]
-    abs_gid = abs(gid)
-
-    link = ''
     name = item[1]
-    preview = ''
-    comment = ''
 
     if 0 == gid:
-        name = '**' + name + '**'
+        end_category()
+
+        # start new category file by writing its header
+        filename = name.replace(':', '').replace(' ', '-')
+        filename = filename.replace('/', 'and').replace('&', 'and')
+        filename = filename.lower() + '.md'
+        outfile = open(toc_path + filename, 'w')
+
+        outfile.write('####{}\n'.format(name))
+        outfile.write('|ID|Name|Preview|Comment|\n|---|---|---|---|\n')
+
+        toc.append('[{}](toc/{})  \n'.format(name, filename))
     else:
+        # write asset row in the table
+        abs_gid = abs(gid)
+
         url = 'https://github.com/alexey-lysiuk/Realm667-AAA-Cache/raw/master/'
         link = '[{0:d}]({1}{0:04d}.zip)'.format(abs_gid, url)
+
         preview = next((ri[3] for ri in remote_repo if abs_gid == ri[0]))
         preview = '![{}](http://www.realm667.com/{})'.format(name, preview)
+
+        comment = ''
 
         if gid < 0:
             link = '~~' + link + '~~'
             name = '~~' + name + '~~'
             comment = 'Excluded from generated package'
 
-    print('|{}|{}|{}|{}|'.format(link, name, preview, comment))
+        if outfile:
+            outfile.write('|{}|{}|{}|{}|\n'.format(link, name, preview, comment))
+
+end_category()
+
+header = (
+    '###Cache for Realm667 - An Awesome Awesomeness\n\n'
+    'Git-based cache of assets from www.realm667.com repository used in '
+    '[Realm667 - An Awesome Awesomeness]'
+    '(https://github.com/alexey-lysiuk/Realm667-AAA) project.\n\n'
+    '####Table of content\n'
+)
+
+with open(utils.cache_path + 'readme.md', 'w') as outfile:
+    outfile.write(header)
+    outfile.writelines(toc)

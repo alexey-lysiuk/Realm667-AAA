6992dd10a1bff09beb92e83814db61d50cc439b6
alexey.lysiuk <alexey.lysiuk@gmail.com>
2016-04-24 14:51:24 +0300

    + generated header for ACS

---
+++ data/zds/scripts/r667zds.h
@@ -0,0 +1,121 @@
+//
+//    Realm667 - ZDoomed Souls
+//    Copyright (C) 2016 Alexey Lysiuk
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#define CLASS_COUNT 35
+
+str CLASS_NAMES[CLASS_COUNT] =
+{
+    "Chainsaw",
+    "Shotgun",
+    "SuperShotgun",
+    "Chaingun",
+    "RocketLauncher",
+    "PlasmaRifle",
+    "BFG9000",
+    "Clip",
+    "ClipBox",
+    "Shell",
+    "ShellBox",
+    "RocketAmmo",
+    "RocketBox",
+    "Cell",
+    "CellPack",
+    "Backpack",
+    "HealthBonus",
+    "Stimpack",
+    "Medikit",
+    "ArmorBonus",
+    "GreenArmor",
+    "BlueArmor",
+    "InvulnerabilitySphere",
+    "Soulsphere",
+    "Megasphere",
+    "BlurSphere",
+    "RadSuit",
+    "Infrared",
+    "Allmap",
+    "Berserk",
+};
+
+int PRICES[CLASS_COUNT] =
+{
+    100,
+    100,
+    200,
+    150,
+    300,
+    300,
+    500,
+    5,
+    25,
+    20,
+    100,
+    10,
+    50,
+    20,
+    100,
+    200,
+    1,
+    10,
+    25,
+    1,
+    100,
+    200,
+    500,
+    200,
+    400,
+    200,
+    100,
+    100,
+    500,
+    250,
+};
+
+int AMOUNTS[CLASS_COUNT] =
+{
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    10,
+    50,
+    4,
+    20,
+    1,
+    5,
+    20,
+    100,
+    1,
+    1,
+    1,
+    25,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+    1,
+};
